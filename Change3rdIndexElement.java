e8a4a7d7e504c9e502d11169d164a7fe6d04d283
Pranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2021-11-16 13:17:53 +0530

    Nov8

---
+++ Change3rdIndexElement.java
@@ -0,0 +1,23 @@
+package assignments;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Change3rdIndexElement {
+	public static void main(String[] args) {
+    	int a[]=new int[5];
+    	
+    	List<Integer> aList = new ArrayList<>();
+    	aList.add(1);
+    	aList.add(2);
+    	aList.add(3);
+    	aList.add(4);
+    	aList.add(5);
+    	aList.add(6);
+    	aList.remove(3);
+    	aList.add(3,8);
+			for(Integer val: aList) {
+    		System.out.println(val);
+    	}
+  }
+
+}
